7d0b59f34f8b0627574e4bc6fe564a3bdc5f2cdc
mollyd13 <mjdaniel@smith.edu>
2022-09-26 19:55:19 -0400

    completed assigned tasks

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,107 @@
-class Conversation {
+//imports all required components from java
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+//runs a conversation between user and computer
+class Conversation {
+  //creates scanner to take user input and array to store transcript
+  static Scanner input = new Scanner(System.in);
+  static ArrayList<String> transcript = new ArrayList<String>();
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    //gets how many rounds the user would like and starts a convo with the proper amount of rounds
+    System.out.print("How many rounds of conversation would you like? ");
+    int numRounds = input.nextInt();
+    input.nextLine();
+    System.out.println("You have chosen " + numRounds + " rounds of conversation.");
+    startConversation(numRounds);
+    }
+
+    public static void startConversation(int numRounds){
+      //computer asks initial question
+      System.out.println("Hi, what's on ur mind?");
+      transcript.add("Hi, what's on ur mind?");
+      
+      //loops through convo for chosen amount of rounds
+      for (int i = 1; i<=numRounds; i++){      
+        String userResp = input.nextLine();
+        transcript.add(userResp);
+        
+        //says hi to the user if they input a greeting
+        if(userResp.equals("hi") || userResp.contains("hello") || userResp.contains("what's up") || userResp.contains("hey")){
+          System.out.println("hi!");
+          transcript.add("hi!");
+        }
+        
+        //if the user asks a question the computer responds that it doesn't know
+        else if (userResp.contains("?")){
+          System.out.println("i'm not sure about that sorry");
+          transcript.add("i'm not sure about that sorry");
+        }
+
+        //mirrors the users words and puts them in question form
+        else if(userResp.contains("i ") || userResp.contains("am ") || userResp.contains("your ")){
+          String newResp = userResp.replace("i ", "you ");
+          newResp = newResp.replace("am ", "are ");
+          newResp = newResp.replace("your ", "my ");
+          System.out.println(newResp + "?");
+          transcript.add(newResp + "?");
+        }
+
+        //mirrors the users words and puts them in question form
+        else if(userResp.contains("you ") || userResp.contains("are ") || userResp.contains("my") || userResp.contains("me ")){
+          String newResp = userResp.replace("you ", "i ");
+          newResp = newResp.replace("me ", "you ");
+          newResp = newResp.replace("are ", "am ");
+          newResp = newResp.replace("my ", "your ");
+          System.out.println(newResp + "?");
+          transcript.add(newResp + "?");
+        }
+
+        //if no mirror words are found, a canned response will be outputted
+        else{
+          System.out.println(createResponse());
+        }
+
+        //once the convo is over, say goodbye and print the transcript
+        if (i == numRounds){
+          System.out.println("Thanks for chatting, see ya!");
+          transcript.add("Thanks for chatting, see ya!");
+          System.out.println("Transcript: ");
+          printTranscript(numRounds);
+          input.close();
+            break;
+          }
+        }
+    }
+
+    //randomly chooses a canned response from an array
+    public static String createResponse(){
+      String[] basicResponses;
+      basicResponses = new String[9];
+      basicResponses[0] = "Wow interesting!";
+      basicResponses[1] = "That's crazy I was just thinking that!";
+      basicResponses[2] = "Awesome!";
+      basicResponses[3] = "Cool.";
+      basicResponses[4] = "Interesting, I've never heard that before.";
+      basicResponses[5] = "Are you sure?";
+      basicResponses[6] = "I know right";
+      basicResponses[7] = "lmao";
+      basicResponses[8] = "seriously?";
+
+      Random generator = new Random();
+      int randomIndex = generator.nextInt(basicResponses.length);
+      String computerResponse = basicResponses[randomIndex];
+      transcript.add(computerResponse);
+      return computerResponse;  
+    }
+
+    //loops through and prints each line of the transcript
+    public static void printTranscript(int numRounds){
+      for (String i : transcript){
+          System.out.println(i);
+      }
+    }
+
   }
-}
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,16 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I consulted Renee at the beginning of this assignment and we brainstormed the outline for our code and began to write some pseudocode. Throughout the rest of the assignment, I consulted with Sydney and we discussed and worked out some specific bugs we were having.
+
 - Which session(s) of TA / office hours did you attend?
+I attended TA hours from 1-2 on Sunday. During this time, I fixed a problem I was having with my transcript. 
+
 - What are your initial impressions of Java? 
+So far, I like it better than python. I like the organization that {} and ; provide. It seems less messy than python and the format of for loops makes much more sense to me. 
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+It seems that debugging is easier in a compiled language like java. Working with both of these languages has proved to me that there are always multiple approaches you can take in order to solve the same problem. Although they may seem very different in regards to syntax, a number of different languages could potentially do the same or at least very similar things. 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+The s4 approach worked really well for me. Writing pseudocode was also critical to my success in this project. It really helped me map out and brainstorm the approach I wanted to take. On the other hand, many times I felt reluctant to ask for help, which led to me having numerous bugs that were easily fixable once I asked for classmates' input. I would advise others taking this course to start with the easy stuff and work their way up to the difficult things, and to always ask for help. Consulting a classmate or TA is more helpful than you may think. 
